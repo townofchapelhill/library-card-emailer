c8cc0e3e644e2c330c36e01d729d411a23c638f5
elizachary99 <elizachary99@gmail.com>
2017-12-19 10:42:56 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import fileinput
+import smtplib
+import datetime
+from datetime import date
+from email.mime.text import MIMEText
+
+secrets = {}
+with fileinput.input("secrets-which-should-not-be-in-an-unsecured-text-file-like-this.txt", inplace = False) as file:
+	secrets["senderUsername"] = file.readline()[:-1]
+	secrets["password"] = file.readline()[:-1]
+	secrets["smtpServer"] = file.readline()[:-1]
+	secrets["smtpPort"] = file.readline()
+	print(secrets)
+
+def days_until(exp_date):
+	e = exp_date.rstrip().split("/")
+	eDate = date(int(e[2]), int(e[0]), int(e[1]))
+	delta = eDate - date.today()
+	return delta.days
+	
+def send(fields):
+	string = "Dear" + fields[2][:-1] + " " + fields[1][1:] + ",\n\nYour library card will expire on " + fields[4] + "."
+	string += "\n\nClick here for instruction on how to renew it.\n\nSincerely,\n\nChapel Hill Public Library"
+	print(string)
+	# Import the email modules we'll need
+	msg = MIMEText(string)
+	# me == the sender's email address
+	# you == the recipient's email address
+	msg['Subject'] = "Library Card Expiration"
+	msg['From'] = secrets["senderUsername"]
+	msg['To'] = fields[-1]
+	# Send the message via SMTP server.
+	# s = smtplib.SMTP(secrets["smtpServer"], secrets["smtpPort"])
+	# s.ehlo()
+	# s.starttls()
+	# s.login(secrets["senderUsername"], secrets["password"])
+	# s.sendmail(msg['From'], msg['To'], msg)
+	# s.quit()
+	
+
+with fileinput.input('PatronsExampleTest.csv', inplace = False) as file:
+	next(file)
+	for line in file:
+		fields = line.rstrip().split(",")
+		if days_until(fields[4]) < 31:
+			send(fields)
